850a9c21143711cd17dba9d4a9faa54642b4f200
Lee Smith <lee.smith@luckie.com>
2011-02-04 14:35:50 -0600

    In the beginning...

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'pg'
+gem 'decent_exposure'
+gem 'haml'
+gem 'haml-rails'
+
+group :development, :test do
+  gem 'capybara'
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'ffaker'
+  gem 'launchy'
+  gem 'rspec-rails'
+  gem 'ruby-debug19'
+  gem 'shoulda'
+  gem 'spork'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DecentAuthentication::Application.config.secret_token = '8b13f6255e9ea393fc7ab32327a76409cd64459fb0cb160daebde557a3d49653f681555d3afd8f2b402f8d0daa5d6f54391311419e2d2c44ed8dba18321baa7a'
