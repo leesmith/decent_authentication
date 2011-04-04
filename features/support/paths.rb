e38ac47636948b4f4046d71cc8b43552e5a0636d
Lee Smith <lee.smith@luckie.com>
2011-04-03 23:31:19 -0500

    Bundle update and cucumber reinstall

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,9 +11,6 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
 
-    when 'the sign in page'
-      sign_in_path
-
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
