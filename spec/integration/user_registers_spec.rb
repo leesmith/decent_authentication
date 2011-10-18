5075b7b2eee0f7748dc42ad28839ac7547c9e411
Lee Smith <lee.smith@luckie.com>
2011-10-18 00:16:44 -0500

    Update rspec

--- spec/integration/user_registers_spec.rb
+++ spec/integration/user_registers_spec.rb
@@ -2,7 +2,7 @@ require 'integration/integration_helper'
 
 describe 'User registers' do
   context 'empty submission' do
-    visit register_path
+    visit '/register'
     click_button 'Register'
     page.should have_content('Email is required')
     page.should have_content('Password is required')
