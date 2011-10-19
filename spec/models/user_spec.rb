3bad115e116f8f13dab40e7441f7fc28b657ddb3
Lee Smith <lee.smith@luckie.com>
2011-10-19 16:13:35 -0500

    Remove password_digest test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,5 @@ describe User do
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
     it { should validate_presence_of :password_confirmation }
-    it { should validate_presence_of :password_digest }
   end
 end
