3bf5a809d617401c9198b19340b2caff457a7f6d
Lee Smith <jeremyleesmith@gmail.com>
2012-03-28 22:39:36 -0500

    Travis ci refactor

---
+++ script/ci/before.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create a database.yml for the right database
+echo "Setting up database.yml for $DB"
+cp config/database.yml.example config/database.yml
+if [ "$DB" = "postgres" ]; then
+  cp config/database.postgres.yml config/database.yml
+elif [ "$DB" = "mysql" ]; then
+  cp config/database.mysql.yml config/database.yml
+fi
+
+# Set up database
+echo "Creating databases for $DB and loading schema"
+bundle exec rake db:create --trace
+bundle exec rake db:schema:load --trace
