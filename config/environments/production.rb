75ec2088280c1dbc8e26d34864f35015a6f38e51
Lee Smith <jeremyleesmith@gmail.com>
2012-10-21 01:01:16 -0500

    Add production mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,18 @@ DecentAuthentication::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'decent-authentication.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :domain         => 'decent-authentication.herokuapp.com',
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD']
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
@@ -64,4 +76,5 @@ DecentAuthentication::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
 end
