610bcb4628f6d0d2cdaebf0b7e90302730860177
Lee Smith <jesmith@temenos.com>
2014-01-14 11:22:18 -0600

    Enforce available locales

--- config/application.rb
+++ config/application.rb
@@ -63,5 +63,7 @@ module DecentAuthentication
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.i18n.enforce_available_locales = true
   end
 end
