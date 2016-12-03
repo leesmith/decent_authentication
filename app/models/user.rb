9fa4c09e21750e2f71bd77dc940cae2ae9d5c086
Lee Smith <jeremyleesmith@gmail.com>
2016-12-03 14:15:30 -0600

    Move all password validations to a validation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,7 @@ class User < ApplicationRecord
   validates :name, :email, presence: true
   validates_uniqueness_of :email, case_sensitive: false
   validates_format_of :email, with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
-  validates_presence_of :password, unless: Proc.new { |u| u.skip_password_validation? }
-  validates_length_of :password, minimum: 8, too_short: 'must be at least 8 characters',
-    unless: Proc.new { |u| u.skip_password_validation? }
-  validates_format_of :password, with: /\A(?=.*[a-z])(?=.*\d).+\z/i,
-    message: 'must be alphanumeric',
-    unless: Proc.new { |u| u.skip_password_validation? }
+  validate :password_requirements, unless: Proc.new { |u| u.skip_password_validation? }
 
   before_save { |user| user.email.downcase! }
 
@@ -19,6 +14,14 @@ class User < ApplicationRecord
 
   def skip_password_validation?
     skip_password_validation
+  end
+
+  private
+
+  def password_requirements
+    validates_presence_of :password
+    validates_length_of :password, minimum: 8, too_short: 'must be at least 8 characters'
+    validates_format_of :password, with: /\A(?=.*[a-z])(?=.*\d).+\z/i, message: 'must be alphanumeric'
   end
 
 end
