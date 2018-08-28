1a1f07a483f64d7198a261f516cceaf8d7b865ce
Lee Smith <jeremyleesmith@gmail.com>
2018-08-28 07:27:48 -0500

    Bundle update

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name 'Tom Jones'
+    name { 'Tom Jones' }
     sequence(:email) { |n| "user#{n}@example.com" }
-    password 'Password000'
-    password_confirmation 'Password000'
-    enabled true
+    password { 'Password000' }
+    password_confirmation { 'Password000' }
+    enabled { true }
   end
 end
