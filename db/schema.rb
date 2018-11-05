b87c480bd1eec1fed158987266833e2d8101ac9b
Lee Smith <jeremyleesmith@gmail.com>
2018-11-04 21:22:46 -0600

    Let rails reformat schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224053424) do
+ActiveRecord::Schema.define(version: 2016_12_24_053424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                                   null: false
-    t.string   "email",                                  null: false
-    t.string   "auth_token"
-    t.string   "password_digest",                        null: false
-    t.string   "password_reset_token"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "auth_token"
+    t.string "password_digest", null: false
+    t.string "password_reset_token"
     t.datetime "password_reset_sent_at"
-    t.boolean  "enabled",                default: false, null: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.boolean "enabled", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "last_sign_in_at"
-    t.index ["auth_token"], name: "index_users_on_auth_token", using: :btree
-    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["auth_token"], name: "index_users_on_auth_token"
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
